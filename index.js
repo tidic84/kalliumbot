1528346b45506db0e293b866779315b6cd95bf94
tidic84 <tidic84@gmail.com>
2021-06-19 15:07:19 +0200

    flemme

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client()
+
+const { token } = require('./config.json')
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+})
+
+client.once('ready', () => {
+    client.user.setActivity('Kallium', { type: 'WATCHING', url:  'https://twitch.tv/'});
+});
+
+client.login(token);
