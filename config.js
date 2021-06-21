93b91dcc572f90d0a0f9a5eecd9936658f5bcf7a
tidic84 <tidic84@gmail.com>
2021-06-21 22:50:35 +0200

    edit db

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   prefix: "!",
   token: "ODU1NTQ1NDM2MzgyMTY3MDgw.YM0Cxw.OzKmaGWJpJQ-zVn77iePMR2Zvng",
-  MONGODB_SRV: 'mongodb+srv://kallium_root:kallium_root_password@kallium.g9dn0.mongodb.net/DiscordBotDB?retryWrites=true&w=majority',
+  MONGODB_SRV: 'mongodb+srv://kallium_root:kallium_root_password@kallium.g9dn0.mongodb.net/KalliumBot-Dev',
   DEFAULTSETTINGS: {
     prefix: "!",
     welcomeMessage: 'Welcome {{user}}'
