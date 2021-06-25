627f763b8e571d1a8ba7cdb4c8ce9a4a320643c0
tidic84 <tidic84@gmail.com>
2021-06-25 19:21:31 +0200

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -3,15 +3,34 @@ const { blue, green, yellow, red } = require('../colors.json')
 
 module.exports = {
     name: 'embed',
-    description: 'template',
+    description: 'embed',
     
-    execute(client, message) {
+    execute(client, message, settings, args, cmd) {
+        let channel ="";
+        channel = args[0]
+        channel = channel.toString()
+        .replace("<", "")
+        .replace(">", "")
+        .replace("#", "");
+        try {
+            channel = client.channels.cache.get(channel)
+        } catch (error) {
+            const embed = new MessageEmbed()
+                .setTitle("Erreur")
+                .setDescription(`'${args[0]}' n'est pas un salon valide`)
+                .setColor(`${red}`)
+            message.channel.send(embed);
+        };
+        const msg = message.content.slice(settings.prefix.length + cmd.length + args[0].length + args[1].length + 3);
+        
+        const title = args[1].replace("/_", " ").replace("/_", " ").replace("/_", " ").replace("/_", " ").replace("/_", " ").replace("/_", " ")
+        
+
         const embed = new MessageEmbed()
-            .setTitle('Heyy !!')
-            .setDescription(`Bienvenue **{{USER}}** !
-            Amuse toi bien sur **{{SERVER_NAME}}** :tada: `)
-            .setImage(message.author.displayAvatarURL({ dynamic : true}))
+            .setAuthor(message.member.displayName, message.author.displayAvatarURL({ dynamic : true }))
+            .setTitle(title)
+            .setDescription(`${msg}`)
             .setColor(`${blue}`)
-        message.channel.send(embed);
+        channel.send(embed);
     }
 };
