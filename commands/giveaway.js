2d5340c1f9dbadfc282f74fc05e7ca6d3e9992db
tidic84 <tidic84@gmail.com>
2021-06-27 21:06:28 +0200

    Update giveaway.js
    update ultra mineur

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -7,12 +7,36 @@ module.exports = {
     description: 'template',
     
     async execute(client, message, settings, args) { 
-// Schéma de commande /giveaway <channel> <temps> <le lot>
+// Schéma de commande /giveaway <channel> <timestamp> <le lot>
+
+        if(args[0] == null) {
+            const embed = new MessageEmbed().setTitle("Erreur").setDescription(`Définit un channel`).setColor(`${red}`)
+            message.channel.send(embed);
+        }
+
+        let channel ="";
+        channel = args[0].toString().replace("<", "").replace(">", "").replace("#", "");
+        try {
+            channel = client.channels.cache.get(channel)
+        } catch (error) {
+            const embed = new MessageEmbed().setTitle("Erreur").setDescription(`'${args[0]}' n'est pas un salon valide`).setColor(`${red}`)
+            message.channel.send(embed);
+        };
+
+        if(args[1] == null) {
+            const embed = new MessageEmbed().setTitle("Erreur").setDescription(`Définit un temps`).setColor(`${red}`)
+            message.channel.send(embed);
+        }
+        if(isNaN(args[1])) {
+            const embed = new MessageEmbed().setTitle("Erreur").setDescription(`${args[1]} n'est pas un nombe valide`).setColor(`${red}`)
+            message.channel.send(embed);
+        }
+        temps = args[1]
 
 
-        // const embed = new MessageEmbed()
-        //     .setTitle('Pong !')
-        //     .setColor(`${blue}`)
-        // message.channel.send(embed);
+        const embed = new MessageEmbed()
+            .setTitle('Pong !')
+            .setColor(`<t:${temps}>:R`)
+        message.channel.send(embed);
     }
 };
